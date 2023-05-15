bcc6eea6a3582813360aed16b66932e3b0a56464
Karen <karenachoyan8@gmail.com>
2023-05-15 18:58:41 +0400

    Front

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,7 +1,10 @@
 const menuToggle = document.querySelector('.menu-toggle');
 const navigation = document.querySelector('.navigation');
+const subm = document.querySelector('.subm');
+const body = document.querySelector('.body');
 
 menuToggle.addEventListener('click', () => {
     menuToggle.classList.toggle('active');
     navigation.classList.toggle('active');
 });
+
--- resources/views/profile/layouts/app.blade.php
+++ resources/views/profile/layouts/app.blade.php
@@ -9,32 +9,46 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="{{ asset('css/profile.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
 <header>
-    <nav>
-        <div class="menu-toggle"></div>
-        <ul class="navigation">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Services</a></li>
-            <li><a href="#">Portfolio</a></li>
-            <li><a href="#">Contact</a></li>
-            <li class="dropdown">
-                <a href="#" class="dropdown-toggle" id="profile-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <i class="fa fa-user-circle"></i>
-                    <span class="profile-name">John Doe</span>
-                </a>
-                <div class="dropdown-menu" aria-labelledby="profile-dropdown">
-                    <a class="dropdown-item" href="#">Change Profile</a>
-                    <a class="dropdown-item" href="#">Logout</a>
+
+            <div class="dokkle">
+                <div>
+              <a href="http://127.0.0.1:8000/">  <img src="images/logo11.png" alt=""></a>
                 </div>
-            </li>
+            </div>
+            <div class="dokkle">
+                <ul>
+                    <li><a href=""></a>Home</li>
+                    <li><a href=""></a>About</li>
+                    <li><a href=""></a>Service</li>
+                    <li><a href=""></a>Portfilo</li>
+                    <li><a href=""></a>Contact</li>
+
+                </ul>
+
+            </div>
+            <div class="dokkle">
+                <span>Admin</span>
+                <i class='fa fa-user-circle'>
+
+                </i>
+
+            </div>
+    <div class="subm">
+        <div class="subm1"></div>
+        <ul>
+            <li>Profile</li>
+            <li>log out</li>
         </ul>
-    </nav>
+    </div>
+
+
 </header>
 
-      <div class="content mt-5">
+                  <div class="content mt-5">
           @yield('content')
       </div>
 </body>
